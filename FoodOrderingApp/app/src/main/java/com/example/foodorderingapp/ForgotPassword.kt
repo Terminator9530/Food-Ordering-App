4e2cd3f221871a50c310d9689692acf24c00d5a2
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-07-27 22:51:35 +0530

    updated forgot password and otp functionality

--- FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/ForgotPassword.kt
+++ FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/ForgotPassword.kt
@@ -10,18 +10,76 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.lang.Exception
 
 class ForgotPassword : AppCompatActivity() {
     lateinit var btnNext: Button
+    lateinit var etMobileNumber: EditText
+    lateinit var etEmail: EditText
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences =
+            getSharedPreferences(getString(R.string.userDetails), Context.MODE_PRIVATE)
         setContentView(R.layout.activity_forgot_password)
         btnNext = findViewById(R.id.btnNext)
+        etMobileNumber = findViewById(R.id.etMobileNumber)
+        etEmail = findViewById(R.id.etEmail)
 
         btnNext.setOnClickListener {
-            val intent = Intent(this@ForgotPassword, OTP::class.java)
-            startActivity(intent)
-            finish()
+            val mobile = etMobileNumber.text.toString()
+            val email = etEmail.text.toString()
+            val queue = Volley.newRequestQueue(this@ForgotPassword)
+            val url = "http://13.235.250.119/v2/forgot_password/fetch_result"
+            val jsonParams = JSONObject()
+            jsonParams.put("mobile_number", mobile)
+            jsonParams.put("email", email)
+            val jsonRequest = object : JsonObjectRequest(
+                Request.Method.POST, url, jsonParams,
+                Response.Listener {
+                    println("Response is $it")
+                    val data = it.getJSONObject("data")
+                    println(data)
+                    try {
+                        val success = data.getBoolean("success")
+                        if (success) {
+                            sharedPreferences.edit().putString("mobile", mobile).apply()
+                            val intent = Intent(this@ForgotPassword, OTP::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            val errorMessage = data.getString("errorMessage")
+                            Toast.makeText(
+                                this@ForgotPassword,
+                                errorMessage,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } catch (e: Exception) {
+                        Toast.makeText(
+                            this@ForgotPassword,
+                            "Some unexpected error occured",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                },
+                Response.ErrorListener {
+                    Toast.makeText(this@ForgotPassword, "volley error occured", Toast.LENGTH_SHORT)
+                        .show()
+                }) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Content-type"] = "application/json"
+                    headers["token"] = "590d13b4181c7b"
+                    return headers
+                }
+            }
+            queue.add(jsonRequest)
         }
     }
 }
--- FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/OTP.kt
+++ FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/OTP.kt
@@ -10,10 +10,93 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.lang.Exception
 
 class OTP : AppCompatActivity() {
+    lateinit var etOTP: EditText
+    lateinit var etPassword: EditText
+    lateinit var etConfirmPassword: EditText
+    lateinit var btnSubmit: Button
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences =
+            getSharedPreferences(getString(R.string.userDetails), Context.MODE_PRIVATE)
         setContentView(R.layout.activity_otp_screen)
+        etOTP = findViewById(R.id.etOTP)
+        etPassword = findViewById(R.id.etPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+        btnSubmit = findViewById(R.id.btnSubmit)
+
+        btnSubmit.setOnClickListener {
+            val password = etPassword.text.toString()
+            val confirmPassword = etConfirmPassword.text.toString()
+            val mobile = sharedPreferences.getString("mobile", "no mobile")
+            val otp = etOTP.text.toString()
+            if (password == confirmPassword) {
+                val queue = Volley.newRequestQueue(this@OTP)
+                val url = "http://13.235.250.119/v2/reset_password/fetch_result"
+                val jsonParams = JSONObject()
+                jsonParams.put("mobile_number", mobile)
+                jsonParams.put("password", password)
+                jsonParams.put("otp", otp)
+                val jsonRequest = object : JsonObjectRequest(
+                    Request.Method.POST, url, jsonParams,
+                    Response.Listener {
+                        println("Response is $it")
+                        val data = it.getJSONObject("data")
+                        println(data)
+                        try {
+                            val success = data.getBoolean("success")
+                            if (success) {
+                                val successMessage = data.getString("successMessage")
+                                Toast.makeText(
+                                    this@OTP,
+                                    successMessage,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                val intent = Intent(this@OTP, Login::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                val errorMessage = data.getString("errorMessage")
+                                Toast.makeText(
+                                    this@OTP,
+                                    errorMessage,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        } catch (e: Exception) {
+                            Toast.makeText(
+                                this@OTP,
+                                "Some unexpected error occured",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    },
+                    Response.ErrorListener {
+                        Toast.makeText(this@OTP, "volley error occured", Toast.LENGTH_SHORT).show()
+                    }) {
+                    override fun getHeaders(): MutableMap<String, String> {
+                        val headers = HashMap<String, String>()
+                        headers["Content-type"] = "application/json"
+                        headers["token"] = "590d13b4181c7b"
+                        return headers
+                    }
+                }
+                queue.add(jsonRequest)
+            } else {
+                Toast.makeText(
+                    this@OTP,
+                    "Password Does not match",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
