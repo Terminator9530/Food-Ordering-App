423f567384d2411c24849878298847c80695bab7
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-07-29 14:44:15 +0530

    updated filter

--- FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/fragment/HomeFragment.kt
+++ FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/fragment/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.example.foodorderingapp.fragment
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,15 +14,50 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.foodorderingapp.R
+import com.example.foodorderingapp.activity.Login
 import com.example.foodorderingapp.adapter.HomeRecyclerAdapter
 import com.example.foodorderingapp.model.Restaurant
 import org.json.JSONException
+import java.util.*
+import kotlin.collections.HashMap
 
 class HomeFragment : Fragment() {
 
     lateinit var recyclerHome: RecyclerView
     lateinit var layoutManager: RecyclerView.LayoutManager
     lateinit var recyclerAdapter: HomeRecyclerAdapter
+    var restaurantInfoList = arrayListOf<Restaurant>()
+    var itemSelected: Int = -1
+
+    var ratingComparator = Comparator<Restaurant> { res1, res2 ->
+        if (res1.restaurantRating.compareTo(res2.restaurantRating, true) == 0) {
+            // sort according to name if rating is same
+            res1.restaurantName.compareTo(res2.restaurantName, true)
+        } else {
+            // else sort according to rating
+            res1.restaurantRating.compareTo(res2.restaurantRating, true)
+        }
+    }
+
+    var priceComparatorLowToHigh = Comparator<Restaurant> { res1, res2 ->
+        if (res1.restaurantCostPerPerson.compareTo(res2.restaurantCostPerPerson, true) == 0) {
+            // sort according to name if rating is same
+            res1.restaurantName.compareTo(res2.restaurantName, true)
+        } else {
+            // else sort according to rating
+            res1.restaurantCostPerPerson.compareTo(res2.restaurantCostPerPerson, true)
+        }
+    }
+
+    var priceComparatorHighToLow = Comparator<Restaurant> { res1, res2 ->
+        if (res1.restaurantCostPerPerson.compareTo(res2.restaurantCostPerPerson, true) == 0) {
+            // sort according to name if rating is same
+            res1.restaurantName.compareTo(res2.restaurantName, true)
+        } else {
+            // else sort according to rating
+            res1.restaurantCostPerPerson.compareTo(res2.restaurantCostPerPerson, true)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +65,7 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        setHasOptionsMenu(true)
         recyclerHome = view.findViewById(R.id.recyclerHome)
         layoutManager = LinearLayoutManager(activity)
 
@@ -48,7 +84,6 @@ class HomeFragment : Fragment() {
                     val success = data.getBoolean("success")
                     if (success) {
                         val dataArray = data.getJSONArray("data")
-                        var restaurantInfoList = arrayListOf<Restaurant>()
                         for (i in 0 until dataArray.length()) {
                             val restaurantJsonObject = dataArray.getJSONObject(i)
                             val restaurantObject = Restaurant(
@@ -94,6 +129,42 @@ class HomeFragment : Fragment() {
 
         queue.add(jsonObjectRequest)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater?.inflate(R.menu.menu_home, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item?.itemId
+        if (id == R.id.action_sort) {
+            val dialog = AlertDialog.Builder(activity as Context)
+            dialog.setTitle("Sort By ?")
+            val listItems = arrayOf("Cost(Low to High)", "Cost(High to Low)", "Rating")
+            dialog.setSingleChoiceItems(listItems, -1) { dialogInterface, i ->
+                itemSelected = i
+            }
+            dialog.setPositiveButton("Yes") { text, listener ->
+                if (itemSelected == 0) {
+                    Collections.sort(restaurantInfoList, priceComparatorLowToHigh)
+                }
+                if (itemSelected == 1) {
+                    Collections.sort(restaurantInfoList, priceComparatorHighToLow)
+                    restaurantInfoList.reverse()
+                }
+                if (itemSelected == 2) {
+                    Collections.sort(restaurantInfoList, ratingComparator)
+                    restaurantInfoList.reverse()
+                }
+                recyclerAdapter.notifyDataSetChanged()
+            }
+            dialog.setNegativeButton("No") { text, listener ->
+
+            }
+            dialog.create()
+            dialog.show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
