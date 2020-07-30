1688dfd58f25e6038e6b68ce6253c3982b3c7dda
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-07-30 12:22:01 +0530

    updated order history

---
+++ FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/adapter/OrderRecyclerAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.foodorderingapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodorderingapp.R
+import com.example.foodorderingapp.database.DishEntity
+import com.example.foodorderingapp.model.Dish
+import com.example.foodorderingapp.model.Order
+
+class OrderRecyclerAdapter(
+    val context: Context,
+    val itemList: ArrayList<Order>
+) :
+    RecyclerView.Adapter<OrderRecyclerAdapter.OrderViewHolder>() {
+    class OrderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtName: TextView = view.findViewById(R.id.txtName)
+        val txtDesc: TextView = view.findViewById(R.id.txtDesc)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): OrderViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_order_single_row, parent, false)
+        return OrderViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(
+        holder: OrderViewHolder,
+        position: Int
+    ) {
+        val dish = itemList[position]
+        println(dish)
+        holder.txtName.text = dish.column1
+        holder.txtDesc.text = dish.column2
+    }
+}
